eb548905e6239d666f914ff508a856bc51f12727
omelnyk10 <orest.melnyk@sigma.software>
2025-12-07 18:58:19 +0200

    Fix and add compiled card

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/ha-timeline-card.ts",
+      entry: "src/timeline-card.ts",
       name: "OutageTimelineCard",
       fileName: () => "outage-timeline-card.js",
       formats: ["es"],
